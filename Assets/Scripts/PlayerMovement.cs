914e1e1eb0d98ce32378f3c33ebe74f3bb553673
Sandy <syjy8809@gmail.com>
2024-05-10 09:12:29 +0900

    :tada: 프로젝트 시작

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+
+        
+    }
+}
